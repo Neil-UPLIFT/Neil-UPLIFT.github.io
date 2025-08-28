e0b99c74e40d211bf1487ddca3fa680920286a69
Neil-UPLIFT <neilupliftcares@gmail.com>
2025-08-28 14:48:04 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuration for production - Replace with your actual values
+const API_KEY = 'AIzaSyCvf18utj4jj8FZz_tUtpsajkq-S1kgcxE';
+const SPREADSHEET_ID = '1fxvm4jYaVA640z4V4L3pPxRNFQjwUGdlnyIBWWzV48Q';
+const SHEET_NAME = 'Form Responses 1';
