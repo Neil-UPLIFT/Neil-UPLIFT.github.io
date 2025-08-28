47a0579367f5c5d19b9649aa4691392a63b52688
Neil Piamonte <neilupliftcares@gmail.com>
2025-08-28 16:18:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuration - Replace with your actual values
-const API_KEY = 'AIzaSyCvf18utj4jj8FZz_tUtpsajkq-S1kgcxE';
+const API_KEY = '';
 const SPREADSHEET_ID = '1fxvm4jYaVA640z4V4L3pPxRNFQjwUGdlnyIBWWzV48Q';
 const SHEET_NAME = 'Form Responses 1';
 
